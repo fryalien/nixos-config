42fe1435e729dac4b9e758fd181c863ecdc0f985
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2024-10-19 20:46:48 +0200

    ch

--- vbguest.nix
+++ vbguest.nix
@@ -3,4 +3,6 @@
 {
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.dragAndDrop = true;
+  virtualisation.virtualbox.guest.seamless = true;
+  virtualisation.virtualbox.guest.clipboard = true;
 }
