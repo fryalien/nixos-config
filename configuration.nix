4c57568ffd37f4ecfcb04a614f4f628c5429fbb0
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2024-10-20 14:16:49 +0200

    ch

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
